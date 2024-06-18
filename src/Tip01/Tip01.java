4f004a903cfc50ed51f03b6d7cd3a7b0145b0ac4
ALUNO <ALUNO@LAB04-PC11>
2024-06-18 19:49:33 -0300

    Tip01

---
+++ src/Tip01/Tip01.java
@@ -0,0 +1,37 @@
+package Tip01;
+
+public class Tip01 {
+    public static void main(String[] args) {
+        //Encontre o total individual de todos após impostos (5%) e gorjeta (15%)
+    double pessoa1 = 10.00;
+    double pessoa2 = 12.00;
+    double pessoa3 = 9.00;
+    double pessoa4 = 8.00;
+    double pessoa5 = 7.00;
+    double pessoa6 = 15.00;
+    double pessoa7 = 11.00;
+    double pessoa8 = 30.00;
+    double tax = 0.05;
+    double tip = 0.15;    
+double total1= pessoa1*(1 +tax +tip);    
+        System.out.println(total1); 
+double total2= pessoa2*(1 +tax +tip);    
+        System.out.println(total2); 
+double total3= pessoa3*(1 +tax +tip);    
+        System.out.println(total3);         
+double total4= pessoa4*(1 +tax +tip);    
+        System.out.println(total4);
+double total5= pessoa5*(1 +tax +tip);    
+        System.out.println(total5);
+double total6= pessoa6*(1 +tax +tip);    
+        System.out.println(total6);
+double total7= pessoa7*(1 +tax +tip);    
+        System.out.println(total7);
+double total8= pessoa8*(1 +tax +tip);    
+        System.out.println(total8);         
+        
+        
+        
+        
+    }    
+}
